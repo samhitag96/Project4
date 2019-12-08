1858a53faee33405d7fe12f405248a4862b9e000
samhitag96 <48071643+samhitag96@users.noreply.github.com>
2019-12-08 11:50:10 -0500

    Add files via upload

--- PlacingOrder with css/orderParking.php
+++ PlacingOrder with css/orderParking.php
@@ -8,38 +8,30 @@
             <link rel = "stylesheet" type = "text/css" href = "style.css">
     </head>
     <body>
-    <ul>
-  <li><a href="login.php">Home</a></li>
-  <li><a href="inventory.php">Inventory</a></li>
-  <li><a href="placeCarRental.php">Place Car Rental</a></li>
-  <li><a href="#orderParking.php">Place Parking Order</a></li>
-  <li style="float:right"><a class="active" href="shoppingCart.php">Shopping Cart</a></li>
-</ul>
-       
+    <img src="logo.png" alt="logo">
         <div class = "pay">
-        <h3>Place Order - Parking</h3>
-        <div id = "container-four">
+        <h3>Place Parking Order</h3>
+        
         <form method = "post" action="./pay.php">
         <br><br>
-        <div id = "container">
-          <div id = "center">
             <strong>Please select the type of parking you'd like: </strong>
-            <select name = "parkType">
+            <br><br>
+            <div id = "container">
+            <div class = "select-wrap">
+            <select name = "parkType" class = "select-box">
                 <option>VIP</option>
                 <option>Regular</option>
             </select>
-            </div>
-            </div>
+            <div class = "select-point">v</div>
+</div>
+</div>
             <br><br>
-            <div id = "container-three">
-            <input type = "date" name = "parkingDate">
-            </div>
+            <input type = "date" name = "parkingDate" class ="select-box">
+
+            
             <br><br>
-            <div id = "center3">
             <input type = "submit" value = "Continue to pay">
-           </div>
-        </form>
-        </div>         
+        </form>         
         </div>
     </body>
 
--- PlacingOrder with css/placeCarRental.php
+++ PlacingOrder with css/placeCarRental.php
@@ -1,56 +1,34 @@
 <?php
     session_start();
 ?>
+<?php echo '<link href="styles.css" rel="stylesheet" type="text/css" />'; ?>
 <HTML>
     <head>
+    <link rel = "stylesheet" type = "text/css" href = "style.css">
             <title>Payment</title>
-            <link rel = "stylesheet" type = "text/css" href = "style.css">
+            
     </head>
     <body>
-    <ul>
-  <li><a href="login.php">Home</a></li>
-  <li><a href="inventory.php">Inventory</a></li>
-  <li><a href="placeCarRental.php">Place Car Rental</a></li>
-  <li><a href="#orderParking.php">Place Parking Order</a></li>
-  <li style="float:right"><a class="active" href="shoppingCart.php">Shopping Cart</a></li>
-</ul>
-       
+    <img src="logo.png" alt="logo">
         <div class = "pay">
-        <h3>Place Order - Car Rental</h3>
-        
+        <h3>Place Car Rental Order </h3>
         <form method = "post" action="./orderParking.php">
         <br><br>
-        <div id = "container">
-          <div id = "center">
             <strong>Please select type of car you'd like: </strong>
-            
-            <select name = "carType">
-                <option>Sedan</option>
-                <option>SUV</option>
-                <option>Luxury</option>
-            </select>
-           </div>
-           </div>
-           <div id = "container-two">
-           <div id = "sedan">
-           <img src = "sedan.png" alt = "sedan">
-           <div id = text> <caption> Sedan</caption></div>
-           </div>
-
-           <div id = "luxury">
-           <img src = "luxury.png" alt = "luxiry">
-           <div id = text> <caption> Luxury</caption></div>
-           </div>
-
-           <div id = "suv">
-           <img src = "suv.png" alt = "suv">
-           <div id = text> <caption> SUV</caption></div>
-           </div>
-           </div>
             <br><br>
-            <div id = "center2">
+            <div id = "container">
+            <div class = "select-wrap">
+            <select name = "carType" class = "select-box">
+                <option class = "option">Sedan</option>
+                <option class = "option">SUV</option>
+                <option class = "option">Luxury</option>
+            </select>
+             <div class = "select-point">v</div>
+</div>
+</div>
+            
+            <br><br>
             <input type = "submit" value = "Continue to parking">
-             </div>
         </form>         
         </div>
     </body>
